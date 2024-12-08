8b64ceed07e66c7b960c1b5835797ffe8bd3304b
XPaladiumyX <allegra.yann@gmail.com>
2024-12-08 16:29:25 +0100

    Start

---
+++ src/main/java/skyxnetwork/chatDelay/ChatDelay.java
@@ -0,0 +1,17 @@
+package skyxnetwork.chatDelay;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatDelay extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
